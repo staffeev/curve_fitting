618f50977e588d67276eba8d3ffcad8ca7053d7b
staffeev <st.affeev@yandex.ru>
2023-08-13 17:50:03 +0300

    fixed

--- main.py
+++ main.py
@@ -5,14 +5,36 @@ from numpy import *
 import timeit
 import time
 import multiprocessing as mp
-from objective import FUNCTIONS
+from matplotlib import pyplot
+import objective
+
+
+# def objective(x, a, b, c, d, e, f):
+# 	return a + b / log(x) + c / (log(x) ** 2) + d / (log(x) ** 3) + e / (log(x) ** 4) + f / (log(x) ** 5)
 
 if __name__ == "__main__":
     dataframe = read_excel("points.xls", dtype=np.float64)
     x, y = dataframe["X"], dataframe["Y"]
     pool = mp.Pool(mp.cpu_count())
     start = time.time()
-    results = [pool.apply_async(curve_fit, args=(f, x, y)) for f in FUNCTIONS]
+    names = [i for i in dir(objective) if i.startswith("objective_")]
+    results = [pool.apply_async(curve_fit, args=(eval(f"objective.{f}"), x, y)) for f in names]
     pool.close()
     print(time.time() - start)
-    print(len(results))
+
+
+    pyplot.scatter(x, y)
+    # define a sequence of inputs between the smallest and largest known inputs
+    x_line = np.arange(min(x), max(x), 1)
+    # calculate the output for the range
+    c = 0
+    for x, i in zip(names, results):
+        c += 1
+        try:
+            func = eval(f"objective.{x}")
+            y_line = func(x_line, *i.get()[0])
+            # create a line plot for the mapping function
+            pyplot.plot(x_line, y_line, '--', color='red')
+        except:
+            pass
+    pyplot.show()
--- pprs.py
+++ pprs.py
@@ -6,15 +6,20 @@ coef_replace_exp = r"\g<0>*"
 
 FUNCTION_PATTERN = "lambda x, {}: {}"
 
+FUNCTION_PATTERN = """
+def objective_{}(x, {}):
+    return {}
+"""
+
 RULES = {
     r"-?[x-y]": replace_exp,  # переменная
     r"-?(?<![\d.])[0-9](?![\d.])": replace_exp,  # целое число
-    r"e\^": r"(e)^",  # е в степени
+    r"e\^": r"(np.e)^",  # е в степени
     r"\([ex-y]\)\^\(+-?\w+\)+": replace_exp,  # переменная в числовой степени или e^x или e^-x
     r"ln\(\w+\)": replace_exp,  # натуральный логарифм
     r"[+-=*/^][a-z]\(": coef_replace_exp,  # коэффициент в уравнении
     r"\(\*": r"*(",  # изменение (* на *(
-    r"ln": r"log", # натуральный логарифм заменяется на log, чтобы numpy мог посчитать
+    r"ln": r"np.log", # натуральный логарифм заменяется на log, чтобы numpy мог посчитать
     r"\)\(": r")*(", # умножение после степени
     r"\^": r"**"  # знак степени
 }
@@ -23,7 +28,7 @@ RULES = {
 def parse_formula(s):
     for exp, repl_exp in RULES.items():
         s = sub(exp, repl_exp, s)
-    return s, list(map(lambda x: x[0], findall(r"[a-z][+*]", s)))
+    return s, list(map(lambda x: x[0], findall(r"[a-z][+*/]", s)))
 
 res = set()
 f = list(map(str.strip, open("formulas3.txt").readlines()))
@@ -32,10 +37,12 @@ c2 = 0
 
 
 with open("objective.py", "a", encoding="utf-8") as file:
-    file.write("FUNCTIONS = [\n")
+    # file.write("FUNCTIONS = [\n")
+    x = 0
     for i in f:
         s, letters = parse_formula(i)
         v1, v2 = s.split("=")
         if v1 == "(y)":
-            file.write("\t" + FUNCTION_PATTERN.format(', '.join(letters), v2) + ',\n')
-    file.write("]")
+            file.write(FUNCTION_PATTERN.format(x, ', '.join(letters), v2))
+            x += 1
+    # file.write("]")
